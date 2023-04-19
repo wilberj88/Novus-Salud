34ad10a4bca43e7ec1988b44d6e32dc700901792
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-04-19 11:15:05 -0500

    Create mental.py

---
+++ pages/mental.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from streamlit_echarts import st_echarts
+import pandas as pd
+import numpy as np
+from pyecharts import options as opts
+from pyecharts.charts import Bar
+from streamlit_echarts import st_pyecharts
+
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Salud", page_icon="🩺")
+
+st.title('Novus Salud 🩺')
+st.header("Soluciones en la Nube para la Salud Mental 🧠")
+
+st.title('Datos del Suicidio en España 2021 📝')
+st.write('Fuente: Observatorio del Suicidio en España (https://www.fsme.es/observatorio-del-suicidio-2021/)')
+
+
+
+st.title('1. Diagnóstico Personalizado 📝')
+
+
+st.title('2. Calendario de Atención Personalizado 📅')
+
+
+st.title('3. Evaluación y Monitoreo Personalizado en Tiempo Real 📍')
