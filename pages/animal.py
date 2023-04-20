c6b9f4b784853a2f94a24d46634814e8146be343
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-04-20 09:12:07 -0500

    Create animal.py

---
+++ pages/animal.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from streamlit_echarts import st_echarts
+import pandas as pd
+import numpy as np
+from pyecharts import options as opts
+from pyecharts.charts import Bar
+from streamlit_echarts import st_pyecharts
+
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Salud", page_icon="🩺")
+
+st.title('Novus Salud 🩺')
