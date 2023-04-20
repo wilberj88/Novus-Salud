7edb643d04d14af403463892af5b68adbc1c70a6
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-04-20 09:22:10 -0500

    Update animal.py

--- pages/animal.py
+++ pages/animal.py
@@ -13,3 +13,14 @@ st.set_page_config(layout="wide", page_title="Novus Salud", page_icon="🩺")
 st.title('Novus Mando - Salud 🩺')
 
 st.header("Soluciones en la Nube para la Salud Animal 🦜")
+st.title('Solución 1: Salvar a los animales de los ensayos 🏥clínicos💉 de 🧪toxicidad☣️')
+st.markdown(
+  """
+  La primera solución para la Salud Animal incluye:
+  - 📝 _    Diagnóstico de tipo de Test Alternativo ante requisitos de moléculas y KPIs
+  - 📆 _    Alarmas de toxicidad por tipos de células
+  - 📍 _    Recomendaciones para minimizar toxicidad
+  
+  EMPIEZA TU CENTRO DE 🩺SALUD ANIMAL🦜 AHORA 🕰
+  """
+)
