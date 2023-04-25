26185b4205a938c7142e38d7b5e6becf40850169
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-04-24 22:38:45 -0500

    Update mental.py

--- pages/mental.py
+++ pages/mental.py
@@ -54,11 +54,10 @@ for secs in range(N,0,-1):
 
 ph2 = st.empty()
 N2 = 485*60
-for secs in range(N,0,-1):
+for secs in range(N2,0,-1):
     mm, ss = secs//60, secs%60
     ph2.metric("Minutos faltantes para el suicidio de una mujer en España", f"{mm:02d}:{ss:02d}")
     time.sleep(1)
-
 
 
 
